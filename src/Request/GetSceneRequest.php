c865446ffc8ac5e55bb932bc06bf0545d9a80ada
Ivan Pepelko <ivan.pepelko@gmail.com>
2021-01-29 13:18:50 +0100

    Add GetSceneRequest

---
+++ src/Request/GetSceneRequest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Flowly\Content\Request;
+
+use Flowly\Content\Request\Part\ResolutionTrait;
+use Symfony\Component\Validator\Constraints as Assert;
+
+/**
+ * @author Ivan Pepelko <ivan.pepelko@gmail.com>
+ */
+class GetSceneRequest
+{
+    use ResolutionTrait;
+
+    /**
+     * @var string
+     * @Assert\Uuid()
+     */
+    private string $id;
+
+    /**
+     * @return string
+     */
+    public function getId(): string
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param string $id
+     */
+    public function setId(string $id): void
+    {
+        $this->id = $id;
+    }
+
+}
